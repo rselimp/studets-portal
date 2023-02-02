2b8009d7391e7bf4bd0165d8ce7f5bac7c32d0bc
rselimp <rselimp@gmail.com>
2023-02-02 20:36:53 +0600

    t

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,24 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import AddStudent from '../AddStudent/AddStudent';
 
 const Home = () => {
     return (
-        <div className="drawer drawer-mobile">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
-                <!-- Page content here -->
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-
-            </div>
-            <div className="drawer-side">
-                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 bg-base-100 text-base-content">
-                    <!-- Sidebar content here -->
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
-                </ul>
-
-            </div>
-        </div>
+       <div>
+        <AddStudent></AddStudent>
+       </div> 
     );
 };
 
